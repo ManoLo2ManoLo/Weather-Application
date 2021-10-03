94f48f6060df2bf314fd413c556ed83d7ea6b4c1
ManoLo2ManoLo <manuel_canas@aol.com>
2021-10-03 01:29:03 -0400

    stylized the webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // Elements that has text contents in it
 const searchEl = document.getElementById('searchEl');
+const dateEl = document.getElementById('dateEl');
 const cityEl = document.getElementById('cityEl');
 const tempEl = document.getElementById('tempEl');
 const windEl = document.getElementById('windEl');
 const humidEl = document.getElementById('humidEl');
 const uvEl = document.getElementById('uvEl');
 const fiveDayForecastEl = document.getElementById('fiveDayForecastEl');
+
+const weatherArea = document.getElementById('weatherArea');
 
 // Elements that are buttons
 const searchBtn = document.getElementById('searchBtn');
@@ -53,14 +56,19 @@ function generateWeather() {
 
 // 
 function displayWeather() {
-    let kelvinToFahrenheit = ((((weatherArray.current.temp - 273.15) * 9) / 5) + 32)
+    let c = weatherArray.current;
+    let kelvinToFahrenheit = ((((c.temp - 273.15) * 9) / 5) + 32)
     let kel2Fah = kelvinToFahrenheit.toFixed(2);
-    let uvIndex = weatherArray.current.uvi
+    let uvIndex = c.uvi
+    let iconUrl = 'http://openweathermap.org/img/w/' + c.weather[0].icon + ".png"
 
+    dateEl.textContent = moment(c.dt, 'X').format('MM/DD/YYYY') + ' ';
+    $('#iconEl').attr('src', iconUrl);
+    console.log(c.weather)
     cityEl.textContent = locationArray.name;
     tempEl.textContent = 'Temp: ' + kel2Fah + '\u00B0F';
-    windEl.textContent = 'Wind: ' + weatherArray.current.wind_speed + ' MPH';
-    humidEl.textContent = 'Humidity: ' + weatherArray.current.humidity + ' %';
+    windEl.textContent = 'Wind: ' + c.wind_speed + ' MPH';
+    humidEl.textContent = 'Humidity: ' + c.humidity + ' %';
     uvEl.textContent = 'UV Index: ' + uvIndex;
     
     $('#uvEl').each(function() {
@@ -75,21 +83,29 @@ function displayWeather() {
 
     var titleEl = document.createElement('p');
     titleEl.innerHTML = '5-Day Forecast:';
+    titleEl.classList = 'title fiveDay';
     fiveDayForecastEl.appendChild(titleEl);
 
-    for (currentDay = 0; currentDay < 5; currentDay++) {
+    for (currentDay = 1; currentDay < 6; currentDay++) {
         let d = weatherArray.daily[currentDay]
         let kelvinToFahrenheit = ((((d.temp.day - 273.15) * 9) / 5) + 32)
         let kel2Fah = kelvinToFahrenheit.toFixed(2);
+        let date = moment(d.dt, 'X')
+        let dayIconUrl = 'http://openweathermap.org/img/w/' + d.weather[0].icon + ".png"
 
         var dayEl = document.createElement('div');
-        dayEl.classList = 'col-2';
+        dayEl.classList = 'col-2 weatherCard';
 
         var dayRowEl = document.createElement('div');
         dayRowEl.classList = 'row';
 
         var dayDateEl = document.createElement('p');
-        dayDateEl.innerHTML = 'Day: ' + (currentDay+1)
+        dayDateEl.innerHTML = date.format('MM/DD/YYYY');
+        dayDateEl.classList = 'miniTitle';
+
+        var dayIconEl = document.createElement('img');
+        dayIconEl.classList = 'icon';
+        $(dayIconEl).attr('src', dayIconUrl);
 
         var dayTempEl = document.createElement('p');
         dayTempEl.innerHTML = 'Temp: ' + kel2Fah + '\u00B0F';
@@ -101,6 +117,7 @@ function displayWeather() {
         dayHumidEl.innerHTML = 'Humidity: ' + d.humidity + " %";
 
         dayRowEl.appendChild(dayDateEl);
+        dayRowEl.appendChild(dayIconEl);
         dayRowEl.appendChild(dayTempEl);
         dayRowEl.appendChild(dayWindEl);
         dayRowEl.appendChild(dayHumidEl);
@@ -110,17 +127,13 @@ function displayWeather() {
 
 }
 
-function timeSetting() {
-    $('#dateEl').text(moment().format('MM/DD/YYYY'))
-}
-
 // Function for when user searches for a city name
 searchBtn.onclick = function() {
     cityName = searchEl.value;
 
     if (cityName) {
+        weatherArea.style.display="block";
         fiveDayForecastEl.innerHTML = '';
         generateLocation();
-        timeSetting();
     }
 }
