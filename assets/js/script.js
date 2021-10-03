e9384f953f623c3ee2e47907a1f470455508b18d
ManoLo2ManoLo <manuel_canas@aol.com>
2021-10-02 20:27:36 -0400

    got the five day forecast running
    just need to editing dates on them

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const tempEl = document.getElementById('tempEl');
 const windEl = document.getElementById('windEl');
 const humidEl = document.getElementById('humidEl');
 const uvEl = document.getElementById('uvEl');
+const fiveDayForecastEl = document.getElementById('fiveDayForecastEl');
 
 // Elements that are buttons
 const searchBtn = document.getElementById('searchBtn');
@@ -13,6 +14,7 @@ const searchBtn = document.getElementById('searchBtn');
 let cityName = '';
 let locationArray = [];
 let weatherArray = [];
+let currentDay;
 
 // Server API to gather Location for the Weather
 function generateLocation() {
@@ -69,7 +71,42 @@ function displayWeather() {
         } else {
             $(this).addClass('severe');
         }
-    })
+    });
+
+    var titleEl = document.createElement('p');
+    titleEl.innerHTML = '5-Day Forecast:';
+    fiveDayForecastEl.appendChild(titleEl);
+
+    for (currentDay = 0; currentDay < 5; currentDay++) {
+        let d = weatherArray.daily[currentDay]
+        let kelvinToFahrenheit = ((((d.temp.day - 273.15) * 9) / 5) + 32)
+        let kel2Fah = kelvinToFahrenheit.toFixed(2);
+
+        var dayEl = document.createElement('div');
+        dayEl.classList = 'col-2';
+
+        var dayRowEl = document.createElement('div');
+        dayRowEl.classList = 'row';
+
+        var dayDateEl = document.createElement('p');
+        dayDateEl.innerHTML = 'Day: ' + (currentDay+1)
+
+        var dayTempEl = document.createElement('p');
+        dayTempEl.innerHTML = 'Temp: ' + kel2Fah + '\u00B0F';
+
+        var dayWindEl = document.createElement('p');
+        dayWindEl.innerHTML = 'Wind: ' + d.wind_speed + ' MPH';
+
+        var dayHumidEl = document.createElement('p');
+        dayHumidEl.innerHTML = 'Humidity: ' + d.humidity + " %";
+
+        dayRowEl.appendChild(dayDateEl);
+        dayRowEl.appendChild(dayTempEl);
+        dayRowEl.appendChild(dayWindEl);
+        dayRowEl.appendChild(dayHumidEl);
+        dayEl.appendChild(dayRowEl);
+        fiveDayForecastEl.appendChild(dayEl);
+    }
 
 }
 
@@ -82,6 +119,7 @@ searchBtn.onclick = function() {
     cityName = searchEl.value;
 
     if (cityName) {
+        fiveDayForecastEl.innerHTML = '';
         generateLocation();
         timeSetting();
     }
